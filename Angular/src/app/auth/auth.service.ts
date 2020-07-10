d6006de7d7ccb1de94712cb31b8b17276d2faebc
Qasim-Alamgir <qaxim12@gmail.com>
2020-07-11 02:00:24 +0500

    changes

--- Angular/src/app/auth/auth.service.ts
+++ Angular/src/app/auth/auth.service.ts
@@ -2,12 +2,13 @@ import { Injectable } from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import { Observable} from 'rxjs';
 import {Datatype} from './datatype';
+import {environment} from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
-  authUrl = "http://localhost:3000/api/";
+  authUrl = environment.API_URL;
 
   constructor(private http: HttpClient) {}  
 authetication(admin):Observable<any>{
